e1587a7aa97509f4f0a3204bd207bb41b454ca9e
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-03-05 07:05:48 +0000

    concat

--- DSA/concatination.js
+++ DSA/concatination.js
@@ -3,10 +3,10 @@ const a = [1, 4, 3, 5, 6];
 
 let j = a.length;
 
-for (let i = 0; i < a.length; i++) {
+for (let i = 0; i < j; i++) {
     a[a.length + i] = a[i]; 
 }
-for (let i = 0; i < a.length; i++) {
+for (let i = 0; i < j; i++) {
    console.log(a[i]); 
     
 }
