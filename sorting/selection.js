a4a82233cbe5b0cf9469f8c6bf01d8dadd92d0dd
Akhileshyadu00 <94223259+Akhileshyadu00@users.noreply.github.com>
2025-03-06 13:23:19 +0000

    selection1

--- sorting/selection.js
+++ sorting/selection.js
@@ -3,5 +3,27 @@ let n = arr.length;
 let temp;
 
 function selectionSort(arr, n) {
-    for
+    for (let i = 0; i < n; i++) {
+       let min_index = i;
+
+       for (let j = i+1; j < n; j++) {
+        if (arr[j] < arr[min_index]) {
+            min_index = j;
+
+        }
+        
+       }
+       temp = arr[min_index];
+       arr[min_index] = arr[i];
+       arr[i] = temp
+        
+    }
+    return arr
 }
+const result = selectionSort(arr, n);
+console.log(result);
+
+
+
+//t- O(n2)
+//s- O(n)
